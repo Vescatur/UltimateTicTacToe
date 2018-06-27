6ce2d850ce8cce137eb58043d3373afe69231078
Vescatur <ivan@edmi.nl>
2018-06-27 21:11:42 +0200

    first

---
+++ UltimateTicTacToeWeb/Model/SuperField.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltimateTicTacToeWeb.Model
+{
+    public class SuperField
+    {
+        public Field[][] fields { get; set; } = {
+           new Field[] { new Field(),new Field(),new Field()},
+           new Field[] { new Field(),new Field(),new Field()},
+           new Field[] { new Field(),new Field(),new Field()}
+        };
+
+        public SquareState winner;
+    }
+}
---
+++ UltimateTicTacToeWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UltimateTicTacToeWeb.Model;
+
+namespace UltimateTicTacToeWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public SuperField superField { get; set; }
+
+        public void OnGet()
+        {
+            superField = new SuperField();
+            superField.fields[0][0].squares[0][0] = SquareState.O;
+            superField.fields[0][0].squares[1][1] = SquareState.X;
+            superField.fields[0][0].squares[1][2] = SquareState.O;
+
+            superField.fields[1][0].squares[0][0] = SquareState.O;
+            superField.fields[1][0].squares[1][1] = SquareState.X;
+            superField.fields[1][0].squares[1][2] = SquareState.O;
+
+            superField.fields[1][2].squares[0][0] = SquareState.O;
+            superField.fields[1][2].squares[1][1] = SquareState.X;
+            superField.fields[1][2].squares[1][2] = SquareState.O;
+        }
+    }
+}
